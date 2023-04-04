c58f67510f637e8575ac6b0f8ea2883e7ad3b528
qxqxia <qxqinxia@outlook.com>
2023-04-04 15:53:59 +0200

    upd: last version

--- assets/sandbox/ex17_scripts/lol.c
+++ assets/sandbox/ex17_scripts/lol.c
@@ -1,6 +1,5 @@
-int	main(int c, char **v)
+int main(int c, char **v)
 {
-	int	i = 0;
-	while (++i < c)
-		printf("%c", (char) atoi(v[i]));
+	int i = 0;
+	while (++i < c) printf("%c", atoi(v[i]));
 }
--- ex07/smallest.c
+++ ex07/smallest.c
@@ -1,1 +1,1 @@
-main(c,v)char**v;{if(c==3)return(printf("%s %s\n",v[1],v[2]),2);}
+main(c,v)char**v;{return c^3?0:(printf("%s %s\n",v[1],v[2]),2);}
