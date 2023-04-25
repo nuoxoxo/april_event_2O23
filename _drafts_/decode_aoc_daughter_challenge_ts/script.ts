4b7c29b7b6ae80a048d8961f86be43448287d7b5
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-04-25 11:21:59 +0200

    p1 : done

--- _drafts_/decode_aoc_daughter_challenge_ts/script.ts
+++ _drafts_/decode_aoc_daughter_challenge_ts/script.ts
@@ -1,28 +1,24 @@
-/*
-import * as fs from "fs";
-
-fs.readFile("filename.txt", "utf8", (err: Error, data: string) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(data);
-});
-*/
-
-/*
-import fs from 'node:fs'
-fs.readFile('p1.in', 'utf8', (err: Error, data: string) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log(data)
-})
-*/
-
 import * as fs from 'fs';
 
-const lines = fs.readFileSync('p1.in', 'utf-8')
-console.log(lines)
+const line = fs.readFileSync('p1.in', 'utf-8')
+console.log(line)
+
+let res: string = ''
+let i: number = 0
+let A = 'A'.charCodeAt(0)
+let Z = 'Z'.charCodeAt(0)
+
+while (i < line.length) {
+    let c = line[i]
+    let code = c.charCodeAt(0)
+    if (((code <= Z && code >= A) || c == '-') &&
+        i < line.length - 1 && c == line[i + 1]) {
+        res += c
+        i += 2
+    } else {
+        ++i
+    }
+}
+
+console.log(res)
+
