97102de03b0c5f8e53ab7effa36c41b428dd8e59
qxqxia <qxqinxia@outlook.com>
2023-04-27 13:57:26 +0200

    + (reviewed) xia qin's regex soln

--- _drafts_/decode_aoc_day_26_using_ts/qxia_soln.js
+++ _drafts_/decode_aoc_day_26_using_ts/qxia_soln.js
@@ -1,22 +1,28 @@
 const fs = require('fs')
-const line = fs.readFileSync('subject.in', 'utf-8')
-//console.log(line)
+const test = fs.readFileSync('test.in', 'utf-8')
+const subj = fs.readFileSync('subject.in', 'utf-8')
 
-function f1(l)
-{
-	//let regex = /([A-Z]|-)\1/g
-	let regex = /(\d)\1/g
+let regex_p1 = /([A-Z]|-)\1/g
+let regex_p2 = /(\d)\1/g
 
+function solve(l, regex) {
 	let result = l.match(regex)
-
-	console.log(result)
-
+	// console.log(result)
 	let s = ""
-
 	for (let i = 0; i < result.length; i++)
 		s += result[i][0];
-
 	return s
 }
 
-console.log(f1(line))
+console.log('test:')
+
+console.log(solve(test, regex_p1))
+console.log(solve(test, regex_p2), '\n')
+
+console.log('subject:')
+console.log(solve(subj, regex_p1))
+console.log(solve(subj, regex_p2))
+
+
+
+
